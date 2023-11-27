6274d4df36a1b8a280ef76f93267e5251cedfa57
AdamHjalmarsson99 <adam.hjalmarsson@student.nbi-handelsakademin.se>
2023-11-27 13:54:03 +0100

    Implemented tests for create, update and delete dog.

--- Infrastructure/Database/MockDatabase.cs
+++ Infrastructure/Database/MockDatabase.cs
@@ -15,7 +15,9 @@ namespace Infrastructure.Database
             new Dog { Id = Guid.NewGuid(), Name = "Björn"},
             new Dog { Id = Guid.NewGuid(), Name = "Patrik"},
             new Dog { Id = Guid.NewGuid(), Name = "Alfred"},
-            new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"}
+            new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"},
+            new Dog { Id = new Guid("4f54036c-2faf-4c3a-b855-1db48d42f2b4"), Name = "TestDogForUpdateTest"},
+            new Dog { Id = new Guid("6ec82d4a-49e7-41b1-a438-845043ee7fc7"), Name = "TestDogForDeleteTest"}
         };
     }
 }

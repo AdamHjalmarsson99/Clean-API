3575b3c4f6cf988de9afe64508fcf85d16cab295
AdamHjalmarsson99 <adam.hjalmarsson@student.nbi-handelsakademin.se>
2023-11-28 13:52:21 +0100

    Implemented tests for cat

---
+++ Test/CatTests/QueryTests/GetCatByIdTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Queries.Cats.GetById;
+using Infrastructure.Database;
+
+namespace Test.CatTests.QueryTests
+{
+    [TestFixture]
+    public class GetCatByIdTests
+    {
+        private GetCatByIdQueryHandler _handler;
+        private MockDatabase _mockDatabase;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _handler = new GetCatByIdQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Handle_ValidId_ReturnsCorrectCat()
+        {
+            //Arrange
+            var catId = new Guid("bf49c5e3-e438-42e7-8f2f-4a6d3656757d");
+            var query = new GetCatByIdQuery(catId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Id, Is.EqualTo(catId));
+        }
+
+        [Test]
+        public async Task Handle_InvalidId_ReturnsNull()
+        {
+            // Arrange
+            var invalidCatId = Guid.NewGuid();
+            var query = new GetCatByIdQuery(invalidCatId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}

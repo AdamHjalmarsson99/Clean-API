1e40520a7e827aeecb058a0d135fae2822ddf5ea
AdamHjalmarsson99 <adam.hjalmarsson@student.nbi-handelsakademin.se>
2023-11-28 13:28:25 +0100

    small white space fix, forgot dotnet format in powershell...

--- Application/Commands/Cats/AddCat/AddCatCommand.cs
+++ Application/Commands/Cats/AddCat/AddCatCommand.cs
@@ -11,10 +11,10 @@ namespace Application.Commands.Cats.AddCat
 {
     public class AddCatCommand : IRequest<Cat>
     {
-        public AddCatCommand(CatDto newCat) 
+        public AddCatCommand(CatDto newCat)
         {
             NewCat = newCat;
         }
-        public CatDto NewCat { get;}
+        public CatDto NewCat { get; }
     }
 }
